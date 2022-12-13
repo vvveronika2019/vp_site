78d29f45bc76e78380e76b60e819a1e8a8c68baf
Veronika <vvveronika2019@mail.ru>
2022-12-13 18:23:02 +0300

    init

---
+++ src/plugins/vuetify.js
@@ -0,0 +1,12 @@
+import Vue from 'vue';
+import 'font-awesome/css/font-awesome.min.css';
+
+import Vuetify from 'vuetify/lib/framework';
+
+Vue.use(Vuetify);
+
+export default new Vuetify({
+    icons:{
+        iconfont: 'fa4'
+    }
+});
